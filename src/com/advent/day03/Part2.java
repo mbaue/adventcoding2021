70b2fd2c82824c18c08c25c11f1079245e2d6541
mar <mb@mb.com>
2021-12-04 23:09:28 +0100

    Day03 - part 2

--- src/com/advent/day03/Part2.java
+++ src/com/advent/day03/Part2.java
@@ -5,38 +5,148 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * --- Day 3: Binary Diagnostic ---
+ * --- Part Two ---
+ * Next, you should verify the life support rating, which can be determined
+ * by multiplying the oxygen generator rating by the CO2 scrubber rating.
+ * <p>
+ * Both the oxygen generator rating and the CO2 scrubber rating are values
+ * that can be found in your diagnostic report - finding them is the tricky part.
+ * Both values are located using a similar process that involves filtering out values
+ * until only one remains. Before searching for either rating value, start with
+ * the full list of binary numbers from your diagnostic report and consider
+ * just the first bit of those numbers. Then:
+ * <p>
+ * Keep only numbers selected by the bit criteria for the type of rating value
+ * for which you are searching. Discard numbers which do not match the bit criteria.
+ * If you only have one number left, stop; this is the rating value for which you are searching.
+ * Otherwise, repeat the process, considering the next bit to the right.
+ * The bit criteria depends on which type of rating value you want to find:
+ * <p>
+ * To find oxygen generator rating, determine the most common value (0 or 1)
+ * in the current bit position, and keep only numbers with that bit in that position.
+ * If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+ * To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position,
+ * and keep only numbers with that bit in that position. If 0 and 1 are equally common,
+ * keep values with a 0 in the position being considered.
+ * For example, to determine the oxygen generator rating value using the same example diagnostic report from above:
+ * <p>
+ * Start with all 12 numbers and consider only the first bit of each number. There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
+ * Then, consider the second bit of the 7 remaining numbers: there are more 0 bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second position: 10110, 10111, 10101, and 10000.
+ * In the third position, three of the four numbers have a 1, so keep those three: 10110, 10111, and 10101.
+ * In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
+ * In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
+ * As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
+ * Then, to determine the CO2 scrubber rating value from the same example above:
+ * <p>
+ * Start again with all 12 numbers and consider only the first bit of each number.
+ * There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 0
+ * in the first position: 00100, 01111, 00111, 00010, and 01010.
+ * Then, consider the second bit of the 5 remaining numbers:
+ * there are fewer 1 bits (2) than 0 bits (3), so keep only the 2 numbers
+ * with a 1 in the second position: 01111 and 01010.
+ * In the third position, there are an equal number of 0 bits and 1 bits (one each).
+ * So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
+ * As there is only one number left, stop; the CO2 scrubber rating is 01010, or 10 in decimal.
+ * Finally, to find the life support rating, multiply the oxygen generator rating (23)
+ * by the CO2 scrubber rating (10) to get 230.
+ * <p>
+ * Use the binary numbers in your diagnostic report to calculate
+ * the oxygen generator rating and CO2 scrubber rating, then multiply them together.
+ * What is the life support rating of the submarine? (Be sure to represent your answer in decimal, not binary.)
+ * <p>
+ * Your puzzle answer was 3277956.
+ */
 
 public class Part2 {
 
+    static List<String> linesWithOne = new ArrayList<>();
+    static List<String> linesWithZero = new ArrayList<>();
+
     public static void main(String[] args) {
-
-
-
-        List<String> lines = new ArrayList<>();
 
         try {
             File file = new File("resources/adv03.txt");
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
 
-            int[] pole = new int[12];
-
             while ((line = br.readLine()) != null) {
-                lines.add(line);
-
+                if (line.charAt(0) == '0') {
+                    linesWithZero.add(line);
+                } else {
+                    linesWithOne.add(line);
+                }
             }
 
-
+            int oxygenOeneratorRating;
+            int co2ScrubberRating;
+            if (linesWithZero.size() > linesWithOne.size()) {
+                oxygenOeneratorRating = processMostCommon(linesWithZero);
+                co2ScrubberRating = processLeastCommon(linesWithOne);
+            } else {
+                co2ScrubberRating = processLeastCommon(linesWithZero);
+                oxygenOeneratorRating = processMostCommon(linesWithOne);
+            }
 
             System.out.println("-------------------SOLUTION-------------------------------------");
-            //System.out.println("solution = " + (gammaRate * epsilonRate));
+            System.out.println("solution = " + (co2ScrubberRating * oxygenOeneratorRating));
             System.out.println("----------------------------------------------------------------");
 
             br.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static char findMostCommonBit(List<String> list, int position) {
+        int ones = 0;
+        int zeros = 0;
+        for (String s : list) {
+            if (s.charAt(position) == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+        if (zeros > ones) {
+            return '0';
+        }
+        return '1';
+    }
+
+    public static int processMostCommon(List<String> list) {
+        int position = 0;
+        do {
+            position++;
+            char common = findMostCommonBit(list, position);
+            ListIterator<String> liter = list.listIterator();
+            while (liter.hasNext()) {
+                if (liter.next().charAt(position) != common) {
+                    liter.remove();
+                }
+            }
+        }
+        while (list.size() > 1);
+        return Integer.valueOf(list.get(0), 2);
+    }
+
+    public static int processLeastCommon(List<String> list) {
+        int position = 0;
+        do {
+            position++;
+            char common = findMostCommonBit(list, position);
+            ListIterator<String> liter = list.listIterator();
+            while (liter.hasNext()) {
+                if (liter.next().charAt(position) == common) {
+                    liter.remove();
+                }
+            }
+        }
+        while (list.size() > 1);
+        return Integer.valueOf(list.get(0), 2);
     }
 }
